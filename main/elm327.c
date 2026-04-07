f2a2a5d264b68cdbf6ba7bd3cd04b56645c87eb8
8dcc <8dcc.git@gmail.com>
2026-04-07 19:47:57 +0200

    Hexdump ELM327 and OBD2 responses

--- main/elm327.c
+++ main/elm327.c
@@ -46,7 +46,11 @@ bool elm327_init(elm327_write_fn write, elm327_read_fn read) {
             return false;
 
         const size_t num_read = read(buf, sizeof(buf) - 1, timeout);
+        assert(num_read < sizeof(buf) - 1);
         buf[num_read]         = '\0';
+
+        LOGD("elm327_init: response to '%s' (%zu bytes):", cmd, num_read);
+        LOGD_HEXDUMP(buf, num_read);
 
         if (strstr((const char*)buf, expected) == NULL)
             return false;
